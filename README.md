3841ea8d64587b2236cf667058ce88ca04eb40c9
Victor Barros <96198078+victorb26@users.noreply.github.com>
2022-03-11 13:27:56 -0300

    Adc README

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align='center'>
+    Personagens da Marvel
+</h1>
+
+# 🤔 Sobre
+A ideia deste pequeno projeto, é a prática de requisição de API, CSS e JavaScript. 
+
+Como mostra a imagem abaixo, foi utilizado Flexbox para a organização das imagens. 
+Este foi o resultado obtido: 
+<img src= "././Images/marvel1.png">
+
+Também foi otimizado para dispositivos mobile:
+<img src= "././Images/marvel2.png">
+
+# 💻Tecnologias utilizadas
+
+- [HTML5](https://developer.mozilla.org/pt-BR/docs/Web/HTML/HTML5)
+- [CSS3](https://www.w3.org/Style/CSS/Overview.en.html)
+- [JavaScript](https://www.javascript.com/)
+- [API Marvel](https://developer.marvel.com/)
+
+
+# 😮 Preview
+
+Para visualizar por completo, clique aqui: (https://victorb26.github.io/API-da-Marvel/) 
+
--- marvel.js
+++ marvel.js
@@ -2,7 +2,7 @@ const timeStamp = '1645124138';
 const apiKey = '8a2fadc476d0fc20630892a3c0890c97';
 const md5 = '02b3febd93a146ddf2c4cdd8085e1c82';
 
-fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=100`
+fetch(`http://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=100`
 ).then((resposta)=> {
     return resposta.json();
 }).then((jsonParsed) =>{
@@ -10,7 +10,7 @@ fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${
 console.log(jsonParsed)
    jsonParsed.data.results.forEach(element => {
        
-    if (element.thumbnail.path != "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" && element.thumbnail.path != "https://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708" ) {
+    if (element.thumbnail.path != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" && element.thumbnail.path != "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708" ) {
         const imageSrc = element.thumbnail.path +'.'+ element.thumbnail.extension
         const nameHero = element.name
         createDivHero(imageSrc, nameHero, divHero);
